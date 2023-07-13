1641e54475db166169d759c47262f890353428ca
drick-infinity <harshimalviya47487@gmail.com>
2023-07-12 20:23:09 -0800

    header starting

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,11 +13,13 @@ const Navbar = () => {
     <img src={images.gericht} alt="app logo"/>
     </div>
     <ul className='app__navbar-links'>
+
       <li className='p__opensans'><a href='#home'>Home</a></li>
       <li className='p__opensans'><a href='#about'>About</a></li>
       <li className='p__opensans'><a href='#menu'>Menu</a></li>
       <li className='p__opensans'><a href='#awards'>Awards</a></li>
       <li className='p__opensans'><a href='#contact'>Contact</a></li>
+    
     </ul>
     <div className='app__navbar-login'>
     <a href='#login' className='p__opensans'>Log In/ Register</a>
@@ -29,7 +31,7 @@ const Navbar = () => {
         {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
         <MdOutlineRestaurantMenu fontSize={25} className='overlay__close' onClick={() => setToggleMenu(false)}/>
-    <ul className='app__navbar-smallscreen-links'>
+    <ul className='app__navbar-smallscreen_links'>
       <li className='p__opensans'><a href='#home'>Home</a></li>
       <li className='p__opensans'><a href='#about'>About</a></li>
       <li className='p__opensans'><a href='#menu'>Menu</a></li>
