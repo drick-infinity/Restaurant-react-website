99e1d76e34aaa55aee4cb26bd04d4dfa2ba093d2
drick-infinity <harshimalviya47487@gmail.com>
2023-07-12 18:46:52 -0800

    navbar toggle and etc things are done

--- src/constants/images.js
+++ src/constants/images.js
@@ -20,7 +20,7 @@ import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
-
+//eslint-disable-next-line
 export default {
   bg,
   chef,
