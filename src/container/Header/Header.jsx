034e476c6f490b65e7668635dc3af960aa775c1e
drick-infinity <harshimalviya47487@gmail.com>
2023-07-12 20:22:39 -0800

    lmost Navbar created and started Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import images  from '../../constants/images';
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavour"/>
+      <h1 className='app__header-h1'>The Key to Find Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>abcdefghijkslfelsfeofmweflefeopfel</p>
+    </div>
+    <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
