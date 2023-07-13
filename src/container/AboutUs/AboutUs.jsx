63d00f9a24f6ee944abfef6b71a7a0fac993a673
drick-infinity <harshimalviya47487@gmail.com>
2023-07-12 22:39:15 -0800

    header about us and hostory section are added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import images from '../../constants/images';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+        <img src={images.G} alt='g letter'></img>
+    </div>
+{/* About us */}
+    <div className='app__aboutus-content flex__center'>
+        <div className='app__aboutus-content_about'>
+            <h1 className='headtext__cormorant'>About Us</h1>
+            <img src={images.spoon} alt='about__spoon' className='spoon__img'></img>
+            <p className='p__opensans'>abcdefghijkslfelsfeofmweflefeopfelsdfkfdsfsdlf fdsfjdsjf fsdfjd fdfldfjoeir fesdf frfeorfelkf oefeorf eeoferofeo fejfifwher iewfioefj iejfroiejf refweiriwe reifwierj wieid ewisfdsfhsdjfksjf sfdkfjesifje edfiejfd ejdfwsejd iejfd jfdeifjiew iwjfdiefd ejfdiefj eejfiejf dsfjsefergj ofjdjfsd </p>
+
+       <button type='button' className='custom__button'>Know More</button>
+        </div>
+
+        <div className='app__aboutus-content_knife flex__center'>
+          <img src={images.knife} alt='about_knife'></img>
+        </div>
+        
+        {/* //History */}
+          <div className='app__aboutus-content_history'>
+            <h1 className='headtext__cormorant'>Our History</h1>
+            <img src={images.spoon} alt='about__spoon' className='spoon__img'></img>
+            <p className='p__opensans'>abcdefghijkslfelsfeofmweflefeopfelsdfkfdsfsdlf fdsfjdsjf fsdfjd fdfldfjoeir fesdf frfeorfelkf oefeorf eeoferofeo fejfifwher iewfioefj iejfroiejf refweiriwe reifwierj wieid ewisfdsfhsdjfksjf sfdkfjesifje edfiejfd ejdfwsejd iejfd jfdeifjiew iwjfdiefd ejfdiefj eejfiejf dsfjsefergj ofjdjfsd </p>
+
+       <button type='button' className='custom__button'>Know More</button>
+        </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,10 +8,14 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new flavour"/>
       <h1 className='app__header-h1'>The Key to Find Dining</h1>
-      <p className='p__opensans' style={{margin:'2rem 0'}}>abcdefghijkslfelsfeofmweflefeopfel</p>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>abcdefghijkslfelsfeofmweflefeopfelsdfkfdsfsdlf
+       fdsfjdsjf fsdfjd fdfldfjoeir fesdf frfeorfelkf oefeorf eeoferofeo fejfifwher iewfioefj iejfroiejf 
+       refweiriwe reifwierj wieid ewisfdsfhsdjfksjf sfdkfjesifje edfiejfd ejdfwsejd iejfd jfdeifjiew iwjfdiefd 
+       ejfdiefj eejfiejf dsfjsefergj ofjdjfsd fesjfie feisfjsdjfise sfisefie sijfdie  eifjiiji jiii ijii hiiuiih</p>
+       <button type='button' className='custom__button'>Explore Menu</button>
     </div>
     <div className='app__wrapper_img'>
-
+        <img src={images.welcome} alt='header img'></img>
     </div>
   </div>
 );
